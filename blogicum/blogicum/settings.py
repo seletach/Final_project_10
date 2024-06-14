fe1296a8a4ba14202a88af3453a051722f32ba21
Илья Лукьянов <ilkanov107@gmail.com>
2024-06-14 13:48:07 +0800

    Отправляю на тесты

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -131,4 +131,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 
-EMAIL_FILE_PATH = BASE_DIR / 'sent_emails' 
+EMAIL_FILE_PATH = BASE_DIR / 'sent_emails'
--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -11,12 +11,11 @@ urlpatterns = [
     path('', include('blog.urls', namespace='blog')),
     path('pages/', include('pages.urls', namespace='pages')),
     path('auth/', include('django.contrib.auth.urls')),
-    path('auth/registration/', 
-         CreateView.as_view(
-         template_name='registration/registration_form.html',
-         form_class=UserCreationForm,
-         success_url=reverse_lazy('blog:index')), 
-         name = 'registration'),
+    path('auth/registration/', CreateView.as_view(
+        template_name='registration/registration_form.html',
+        form_class=UserCreationForm,
+        success_url=reverse_lazy('blog:index')),
+        name='registration'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'pages.views.page_not_found'
